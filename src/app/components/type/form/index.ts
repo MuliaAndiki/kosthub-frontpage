855b0c2a3862c2cad6075cd1a74e73c1c950c05d
Mulia Andiki <mulia23@mhs.usk.ac.id>
2025-05-28 02:55:49 +0700

    add Type

--- src/app/components/type/form/index.ts
+++ src/app/components/type/form/index.ts
@@ -13,3 +13,30 @@ export interface formLogin {
   username: string;
   password: string;
 }
+
+export interface formReservase {
+  nama: string;
+  tanggal_lahir: string;
+  nomorHp: string;
+  gender: boolean;
+  email: string;
+  metode_pembayaran: string;
+  kontrak: File;
+  bukti_pembayaran: File;
+}
+
+export interface formEditProfile {
+  fotoProfile: File | null;
+  fullname: string;
+  tanggal_lahir: string;
+  nomor: string;
+  email: string;
+  gender: boolean;
+  bio: string;
+  alamat: string;
+}
+
+export interface formChangePassword {
+  oldPassword: string;
+  newPassword: string;
+}
