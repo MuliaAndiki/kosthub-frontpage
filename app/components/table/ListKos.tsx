7190caa2e31beabc851119e99496cedcbadfea69
diki <muliaandiki@gmaiil.com>
2025-06-29 05:05:54 +0700

    feat: intergrate-api getReservase

--- app/components/table/ListKos.tsx
+++ app/components/table/ListKos.tsx
@@ -23,15 +23,24 @@ const ListKosChildren: React.FC = () => {
     alasan: "",
     status: "",
   });
-  const [isActive, setIsActive] = useState<"approved" | "rejected" | null>(
-    null
-  );
+  const [isActive, setIsActive] = useState<"PopUp" | null>(null);
 
   const approve = ["approved", "rejected"];
 
+  const handleRejected = () => {};
+
   const handleApprove = async () => {
     try {
-      const res = await API.patch(`/api/kos/${idKos?._id}/approve`, {});
+      const res = await API.patch(
+        `/api/kos/${idKos?._id}/approve`,
+        formApprove,
+        {
+          headers: {
+            Authorization: `Bearer ${currentUser?.token}`,
+          },
+        }
+      );
+      console.log(`Berhasil Approve ${res}`);
     } catch (error) {
       console.log(`Gagal Melakukan Approve ${error}`);
     }
@@ -73,110 +82,113 @@ const ListKosChildren: React.FC = () => {
   return (
     <Container className="w-full ">
       <Container className="flex justify-center w-full items-center flex-col ">
-        <Container className="w-full flex flex-col ">
-          {kostDatas?.map((items, key) => (
-            <Container key={key} className="grid grid-cols-6 grid-rows-1 px-4">
-              {isLoading ? (
-                <Pulse className="w-26 rounded-md my-2 " />
-              ) : (
-                <span className="">{items.nama_kos}</span>
-              )}
-              {isLoading ? (
-                <Pulse className="w-26 rounded-md my-2 " />
-              ) : (
-                <span>{items.alamat}</span>
-              )}
-              {isLoading ? (
-                <Pulse className="w-26 rounded-md my-2" />
-              ) : (
-                <span>{items.kontak.nomor}</span>
-              )}
-              {isLoading ? (
-                <Pulse className="w-26 rounded-md my-2" />
-              ) : (
-                <span>{items.status}</span>
-              )}
-              {isLoading ? (
-                <Pulse className="w-26 rounded-md my-2" />
-              ) : (
-                <Button onClick={() => handleRedirect()}>Selanjutnya</Button>
-              )}
-
-              <Container className="flex flex-col justify-center items-center">
-                {isLoading ? (
-                  <Pulse className="w-26 rounded-md my-2" />
-                ) : (
-                  <ButtonPopUp
-                    onClick={() => setIsActive("approved")}
-                    message="success"
-                  >
-                    Approved
-                  </ButtonPopUp>
-                )}
-                {isLoading ? (
-                  <Pulse className="w-26 rounded-md my-2" />
-                ) : (
-                  <ButtonPopUp
-                    onClick={() => setIsActive("rejected")}
-                    message="error"
-                  >
-                    Rejected
-                  </ButtonPopUp>
-                )}
-              </Container>
-              <PopUp
-                isOpen={isActive === "approved"}
-                onClose={() => setIsActive(null)}
-              >
-                <Container className="w-full flex justify-center flex-col gap-4">
-                  <h1>Approved</h1>
-                  <CustomSelect
-                    name="Status"
-                    value={formApprove.status}
-                    onChange={(e) => handleStatusChange(e)}
-                  >
-                    {approve.map((key) => (
-                      <MenuItem key={key} value={key}>
-                        {key}
-                      </MenuItem>
-                    ))}
-                  </CustomSelect>
-                  <TextFieldInput
-                    name={formApprove.alasan}
-                    value={formApprove.alasan}
-                    label="Alasan"
-                    onChange={(e) =>
-                      setFormApprove((prev) => {
-                        const newObj = { ...prev, alasan: e.target.value };
-                        return newObj;
-                      })
-                    }
-                  />
-
-                  <Container className="flex justify-center items-center gap-4">
-                    <ButtonPopUp
-                      message="success"
-                      onClick={() => handleApprove()}
-                    >
-                      Yakin
-                    </ButtonPopUp>
-                    <ButtonPopUp
-                      message="error"
-                      onClick={() => setIsActive(null)}
-                    >
-                      Tidak
-                    </ButtonPopUp>
-                  </Container>
-                </Container>
-              </PopUp>
-              {isActive === "rejected" && (
-                <Container>
-                  <h1>ini PopUp Reject</h1>
-                </Container>
-              )}
+        {isLoading ? (
+          <Container className="flex-col ">
+            <Container className="flex justify-center items-center h-full w-full gap-2">
+              <Container className="w-6 h-6 border-4 border-dashed rounded-full animate-spin border-sky-500 size-105"></Container>
+              <p className="text-[2rem] font-light">Loading...</p>
             </Container>
-          ))}
-        </Container>
+          </Container>
+        ) : (
+          <Container className="w-full flex flex-col ">
+            {kostDatas?.map((items, key) => (
+              <Container
+                key={key}
+                className="grid grid-cols-6 grid-rows-1 px-4 "
+              >
+                {isLoading ? (
+                  <Pulse className="w-26 rounded-md my-2 " />
+                ) : (
+                  <span className="">{items.nama_kos}</span>
+                )}
+                {isLoading ? (
+                  <Pulse className="w-26 rounded-md my-2 " />
+                ) : (
+                  <span>{items.alamat}</span>
+                )}
+                {isLoading ? (
+                  <Pulse className="w-26 rounded-md my-2" />
+                ) : (
+                  <span>{items.kontak.nomor}</span>
+                )}
+                {isLoading ? (
+                  <Pulse className="w-26 rounded-md my-2" />
+                ) : (
+                  <span>{items.status}</span>
+                )}
+                {isLoading ? (
+                  <Pulse className="w-26 rounded-md my-2" />
+                ) : (
+                  <Container className="w-full  ">
+                    <Button onClick={() => handleRedirect()}>
+                      Selanjutnya
+                    </Button>
+                  </Container>
+                )}
+
+                <Container className="flex  justify-center items-center">
+                  {isLoading ? (
+                    <Pulse className="w-26 rounded-md my-2" />
+                  ) : (
+                    <ButtonPopUp
+                      onClick={() => setIsActive("PopUp")}
+                      message="secondary"
+                    >
+                      Ambil Tindakan
+                    </ButtonPopUp>
+                  )}
+                </Container>
+                <PopUp
+                  isOpen={isActive === "PopUp"}
+                  onClose={() => setIsActive(null)}
+                >
+                  <Container className="w-full flex justify-center flex-col gap-4">
+                    <h1 className="text-center md:text-2xl font-bold">
+                      Mohon Lengkapi
+                    </h1>
+
+                    <CustomSelect
+                      name="Status"
+                      value={formApprove.status}
+                      onChange={(e) => handleStatusChange(e)}
+                    >
+                      {approve.map((key) => (
+                        <MenuItem key={key} value={key}>
+                          {key}
+                        </MenuItem>
+                      ))}
+                    </CustomSelect>
+                    <TextFieldInput
+                      name={formApprove.alasan}
+                      value={formApprove.alasan}
+                      label="Alasan"
+                      onChange={(e) =>
+                        setFormApprove((prev) => {
+                          const newObj = { ...prev, alasan: e.target.value };
+                          return newObj;
+                        })
+                      }
+                    />
+                    <Container className="flex justify-center items-center gap-4">
+                      <ButtonPopUp
+                        message="success"
+                        onClick={() => handleApprove()}
+                      >
+                        Yakin
+                      </ButtonPopUp>
+                      <ButtonPopUp
+                        message="error"
+                        onClick={() => setIsActive(null)}
+                      >
+                        Tidak
+                      </ButtonPopUp>
+                    </Container>
+                  </Container>
+                </PopUp>
+              </Container>
+            ))}
+          </Container>
+        )}
       </Container>
     </Container>
   );
